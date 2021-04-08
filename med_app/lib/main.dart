574c4670a4ef2bc84e7d2b5b47b72ba973881b30
abdelrhmanadel2 <47029136+abdelrhmanadel2@users.noreply.github.com>
2021-04-08 18:49:10 +0200

    remove default code

--- med_app/lib/main.dart
+++ med_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:med_app/UI/splash/splash_screen.dart';
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+
       home: Splash(),
-    );
+
   }
 }
