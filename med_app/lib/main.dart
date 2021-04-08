a73696c5ab40f254de9054812172033e496ee833
abdelrhmanadel2 <47029136+abdelrhmanadel2@users.noreply.github.com>
2021-04-09 01:50:48 +0200

    add login, signup navigation

--- med_app/lib/main.dart
+++ med_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:med_app/UI/splash/splash_screen.dart';
+import 'package:med_app/UI/Login.dart';
+import 'package:med_app/UI/Signup.dart';
 
 void main() {
   runApp(MyApp());
