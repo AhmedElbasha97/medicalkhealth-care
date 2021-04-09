1eb219ca766541547b1c8893f8ec4c15fa811a95
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2021-04-09 04:57:16 +0200

    Revert "Merge pull request #4 from AhmedElbasha97/Abdelrhman"
    This reverts commit 21b6c0181461361e4e15a9b1cf68d4e832728ad5, reversing
    changes made to 3e01a00869c361001a3caed989f0320f78baac59.

--- med_app/lib/Styles/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class ColorsCollection {
-  const ColorsCollection();
-
-  static const Color mainColor = const Color(0xFF00A1A7);
-  static const Color secondaryColor = const Color(0xff184858);
-  static const Color splashTitleColor = const Color(0xff184858);
-}
