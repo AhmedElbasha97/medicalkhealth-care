7c854dd6b38dcef1530a5398085ed5ccd619ca28
abdelrhmanadel2 <47029136+abdelrhmanadel2@users.noreply.github.com>
2021-04-10 05:02:57 +0200

    Update rounderd_small_button.dart

--- med_app/lib/Widgets/rounderd_small_button.dart
+++ med_app/lib/Widgets/rounderd_small_button.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:med_app/Styles/colors.dart';
+import 'package:med_app/services/auth.dart';
+import 'package:provider/provider.dart';
 
 class RoundedSmallButton extends StatelessWidget {
   final String text;
-  final navigation;
-  RoundedSmallButton({this.text, this.navigation});
+  final  password;
+  final email;
+  final String confirm_password;
+  final String username;
+  final type;
+  RoundedSmallButton({this.text, this.password,this.email,this.username,this.confirm_password,this.type});
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
@@ -14,21 +20,37 @@ class RoundedSmallButton extends StatelessWidget {
           width: constraints.maxWidth - 150,
           height: 50,
           child: RaisedButton(
-            onPressed: () {
+            onPressed: () async{
               // Navigate to the second screen using a named route.
-              // Navigator.pushNamed(context, '/${this.navigation}');
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => this.navigation));
-            },
+              // Navigator.pushNamed(context, '/${this.navigation}');       
+                  switch (type){
+                    case 'login':
+                     context.read<AuthService>().signInEmail(context,email, password);
+                     break;
+                     case 'doctor':
+                       context.read<AuthService>().signUpEmail(context:context,email:email,password:password,confirmpassword:confirm_password,username:username,type:type);
+                      break;
+                    case 'user':
+                       context.read<AuthService>().signUpEmail(context:context,email:email,password:password,confirmpassword:confirm_password,username:username,type:type);
+                      break;
+                      }
+                  
+                  
+                      },
             child: Text(
               text,
               style: TextStyle(
                 fontSize: 18,
               ),
             ),
-            color: ColorsCollection.mainColor,
-            textColor: Colors.white,
+            color: (type=='doctor')? ColorsCollection.mainColor : Colors.white,
+            textColor: (type=='doctor')?   Colors.white:ColorsCollection.mainColor,
+            
             shape: RoundedRectangleBorder(
+              side: BorderSide(
+              color: ColorsCollection.mainColor,
+              width: 1,
+              style: BorderStyle.solid),
               borderRadius: BorderRadius.circular(30.0),
             ),
           ),
@@ -37,3 +59,4 @@ class RoundedSmallButton extends StatelessWidget {
     });
   }
 }
+
