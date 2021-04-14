198b5d85b3a6336db2f50bbdcadfae5a06561fca
abdelrhmanadel2 <47029136+abdelrhmanadel2@users.noreply.github.com>
2021-04-14 21:57:54 +0200

    Revert "update database"

--- med_app/lib/providedrs/database_provider.dart
+++ med_app/lib/providedrs/database_provider.dart
@@ -22,8 +22,8 @@ class DatabaseProvider extends ChangeNotifier {
   }
 
   void getDoctorById(id) {
-    _database_repositorie.fetchDoctor(id).then((doc) {
-      this.doctor = doc;
+    _database_repositorie.fetchDoctor(id).then((doctor) {
+      this.doctor = doctor;
       notifyListeners();
     });
   }
--- med_app/lib/services/database.dart
+++ med_app/lib/services/database.dart
@@ -6,39 +6,93 @@ class DataBaseService {
 
   final ref = FirebaseDatabase.instance.reference();
 
+
   Future<List<Doctor>> fetchDoctorsBySpecialty(speciality) async {
     Map<dynamic, dynamic> values;
     List<Map<dynamic, dynamic>> retrievedName = [];
     List<Doctor> doctors = [];
+
     await ref
-        .child('users/doctors')
-        .orderByChild("speciality")
+        .child('Uesrs/Doctors')
+        .orderByChild("Speciality")
         .equalTo(speciality)
         .once()
         .then((DataSnapshot data) {
       values = data.value;
+      
     });
     if (values != null) {
-      values.forEach((key, values) {
-        doctors.add(Doctor.fromJson(values));
-      });
+        values.forEach((key, values) {
+          retrievedName.add(values);
+     
+  
+          doctors.add(Doctor(
+            appointment:
+                (values["Appointment"] != "") ? values["Appointment"] : [],
+            appointmentAv: (values["Appointment-AV"] != "")
+                ? values["Appointment-AV"]
+                : [],
+            bio: values["Bio"],
+            documents: Documents(
+                candidate: values["Documents"]["Candidate"],
+                certificate: values["Documents"]["Certificate"],
+                id: values["Documents"]["ID"]),
+            speciality: values["Speciality"],
+            doctorAvatare: (values["Doctor-Avatar"]!="")? values["Doctor-Avatar"]:"https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+            userId: values["User-Id"],
+            nameUser: values["name-user"],
+          ));
+        });
+        print(doctors);
       return doctors;
-    } else {
-      return null;
-    }
+      }
+      else{
+         return null;
+      }
+    
+    
+  
   }
 
-  Future<Doctor> fetchDoctor(id) async {
+Future<Doctor> fetchDoctor(id) async {
     Map<dynamic, dynamic> values;
-    Doctor doctor;
-    await ref.child('users/doctors/$id').once().then((DataSnapshot data) {
+    Map<dynamic, dynamic> retrievedName ;
+    Doctor doctor ;
+
+    await ref
+        .child('Uesrs/Doctors')
+        .orderByChild("User-Id")
+        .equalTo(id)
+        .once()
+        .then((DataSnapshot data) {
       values = data.value;
-    });
-    if (values != null) {
-      doctor = Doctor.fromJson(values);
+      if (values != null) {
+        values.forEach((key, values) {
+          retrievedName=values;
+          doctor=Doctor(
+            appointment:
+                (values["Appointment"] != "") ? values["Appointment"] : [],
+            appointmentAv: (values["Appointment-AV"] != "")
+                ? values["Appointment-AV"]
+                : [],
+            bio: values["Bio"],
+            documents: Documents(
+                candidate: values["Documents"]["Candidate"],
+                certificate: values["Documents"]["Certificate"],
+                id: values["Documents"]["ID"]),
+            speciality: values["Speciality"],
+            doctorAvatare:(values["Doctor-Avatar"]!="")? values["Doctor-Avatar"]:"https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+            userId: values["User-Id"],
+            nameUser: values["nameUser"],
+          );
+        });
+        print(doctor);
       return doctor;
-    } else {
-      return null;
-    }
+      }
+      else{
+            throw Exception('FAILED TO Fetch doctor with this $id');
+        
+      }
+    });
   }
 }
